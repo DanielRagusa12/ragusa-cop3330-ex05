09ace95d52027755f1cec3f19a50fde98ae9f490
DanielRagusa12 <daniel.ragusa129@gmail.com>
2021-09-13 22:57:35 -0400

    Done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,7 @@
 package org.example;
 
 /*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
  *  Copyright 2021 Daniel Ragusa
  */
 
@@ -15,20 +15,22 @@ public class App
         System.out.print("What is the first number? ");
         Scanner input=new Scanner(System.in);
         String_1=input.next();
-        int Num_1=Integer.parseInt(String_1);
 
         String String_2;
         System.out.print("What is the second number? ");
         Scanner input2=new Scanner(System.in);
         String_2=input2.next();
+
+        int Num_1=Integer.parseInt(String_1);
         int Num_2=Integer.parseInt(String_2);
 
         int add=Num_1+Num_2;
         int subtract=Num_1-Num_2;
         int multiply=Num_1*Num_2;
         int divide=Num_1/Num_2;
+        String Final=((String_1 )+ " + " +(String_2 )+ " = "+( add)+"\n"+(String_1 )+ " - " +(String_2 )+ " = "+( subtract)+"\n"+(String_1 )+ " * " +(String_2 )+ " = "+( multiply)+"\n"+(String_1 )+ " / " +(String_2 )+ " = "+( divide)+"\n");
 
-        System.out.print((String_1 )+ "+" +(String_2 )+ "="+( add)+"\n"+(String_1 )+ "-" +(String_2 )+ "="+( subtract)+"\n"+(String_1 )+ "*" +(String_2 )+ "="+( multiply)+"\n"+(String_1 )+ "/" +(String_2 )+ "="+( divide)+"\n");
+        System.out.print(Final);
 
 
     }
